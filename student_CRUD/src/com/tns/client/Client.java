573d4ca5943dc7efed7719e7230db45d7ed92623
abc <abc@abc-Inspiron-15-3567>
2022-04-28 12:49:48 +0530

    Student_CRUD Operation

---
+++ student_CRUD/src/com/tns/client/Client.java
@@ -0,0 +1,44 @@
+package com.tns.client;
+import com.tns.entities.Student;
+import com.tns.service.StudentService;
+import com.tns.service.StudentServiceImpl;
+
+public class Client {
+
+	public static void main(String[] args) {
+		
+		
+		StudentService service = new StudentServiceImpl();
+		
+		Student student = new Student();
+		
+		// Create Operation
+		student.setStudentId(100);
+		student.setName("Tejashree");
+		service.addStudent(student);
+		
+		/*
+		// Retrieve Operation
+		student = service.findStudentById(100);
+		System.out.print("ID:"+student.getStudentId());
+		System.out.println("Name:"+student.getName());
+		
+		// Update Operation
+		student = service.findStudentById(100);
+		student.setName("Rudra");
+		service.updateStudent(student);
+		
+		//at this breakpoint, we have updated record added in first section
+		student = service.findStudentById(100);
+		System.out.print("ID:"+student.getStudentId());
+		System.out.println(" Name:"+student.getName());
+		
+		//at this breakpoint, record is removed from table
+		// Delete Operation
+		student = service.findStudentById(100);
+		service.removeStudent(student);
+		System.out.println("End of program");
+*/
+	}
+
+}
